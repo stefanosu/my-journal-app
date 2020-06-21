aacd405ccd7b669a1732b9f72ebdac9dc83950d5
Stefanos <stefanosu422@gmail.com>
2020-06-20 20:11:24 -0400

    added before action and private method for lowercase

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::API 
+  before_action :underscore_params!
+  protect_from_forgery with: :null_session  
+
+
+  def underscore_params!
+    params.deep_transform_keys!(&:underscore)
+  end
+
 end
