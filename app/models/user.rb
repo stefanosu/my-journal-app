ed9259cc42bbecd5f4cb0a5209cf19d20abd7fd5
Stefanos <stefanosu422@gmail.com>
2020-08-13 13:55:43 -0400

    added validations on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+    
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
   def generate_jwt 
     JWT.encode({ id: id, 
                 exp: 60.days.from.now.to_i},
                 Rails.application.secrets.secret_key_base)
   end
+
+
+
 end
