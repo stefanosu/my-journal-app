c4b73134fbcfc1686d6733bd3624b59c0b3ebd10
Stefanos <stefanosu422@gmail.com>
2020-08-13 13:04:10 -0400

    added method to wrap devise routes in a scope api block to json by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  scope :api, defaults: { format: :json } do 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  
+  end
 end
