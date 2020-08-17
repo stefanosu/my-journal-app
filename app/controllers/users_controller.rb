c8a61c6c491e117e37192c206237018fec66ffe4
Stefanos <stefanosu422@gmail.com>
2020-08-17 14:10:51 -0400

    added show and update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController 
   before_action :authenticate_user! 
 
+def show
+
+end
 
 
-
-
+def update
+  if current_user_update_attributes(user_params) 
+    render :show 
+  else
+    render json: { errors: current_user.errors }, status: :unprocessable_entity   
+  end
+end
 
 
 
