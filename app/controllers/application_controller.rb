8d17fe2e87794f1f4a5c14af739567ede9e2db2f
Stefanos <stefanosu422@gmail.com>
2020-08-17 13:24:00 -0400

    added method to authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API 
   before_action :underscore_params!
   before_action :configured_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user 
   protect_from_forgery with: :null_session
   respond_to :json
 
@@ -12,8 +13,20 @@ class ApplicationController < ActionController::API
   private 
 
   def configured_permitted_parameters 
-    devise_paramater_sanitizer.permit(:sign_up, keys:[:username])
+    devise_paramater_sanitizer.for(:sign_up) << :username
   end
 
+  def authenticate_user
+    if request.headers['Authorization'].present?
+      authenticate_or_request_with_http_token do |token|
+        begin
+          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
 
-end
+          @current_user_id = jwt_payload['id']
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+          head :unauthorized
+        end
+      end
+    end
+  end
+end 
