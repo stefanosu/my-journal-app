4bce26f5beb910a33f5810cf745fabb796990bc7
Stefanos <stefanosu422@gmail.com>
2020-08-17 13:09:51 -0400

    added private method to configure permitted paramaters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API 
   before_action :underscore_params!
+  before_action :configured_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session
   respond_to :json
 
@@ -7,5 +8,12 @@ class ApplicationController < ActionController::API
   def underscore_params!
     params.deep_transform_keys!(&:underscore)
   end
+
+  private 
+
+  def configured_permitted_parameters 
+    devise_paramater_sanitizer.permit(:sign_up, keys:[:username])
+  end
+
 
 end
