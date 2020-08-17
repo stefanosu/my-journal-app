6c6f5cee5b50e020d4ce7a7e3300f47a9983fbb6
Stefanos <stefanosu422@gmail.com>
2020-08-17 11:58:31 -0400

    added user login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do 
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {sessions: :sessions}, 
+  path_names: {sign_in: :login} 
   end
 end
