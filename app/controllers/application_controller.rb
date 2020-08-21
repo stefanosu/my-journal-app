16df69cafd772879eceb9bce36422353fc4c884d
Stefanos <stefanosu422@gmail.com>
2020-08-21 19:23:54 -0400

    need to fix 500 internal server error authenticate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,10 +21,10 @@ class ApplicationController < ActionController::API
 
   def authenticate_user 
     if request.headers['Authorization'].present?
+      # byebug
       authenticate_or_request_with_http_token do |token| 
         begin
           jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
-
           @current_user_id = jwt_payload['id']
         rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
           head :unauthorized
