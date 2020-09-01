dad1f22c30d96b36639a00aa8fe4acd54cbca625
Stefanos <stefanosu422@gmail.com>
2020-08-31 21:04:14 -0400

    updated app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,11 +22,11 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user 
     if request.headers['Authorization'].present?
-      # byebug
       authenticate_or_request_with_http_token do |token| 
         begin
           jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
           @current_user_id = jwt_payload['id']
+          byebug
         rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
           head :unauthorized
         end
