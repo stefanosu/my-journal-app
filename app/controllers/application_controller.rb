c344be58e864d6cb83e91452c617cfa597c8e99e
Stefanos <stefanosu422@gmail.com>
2020-09-01 21:20:37 -0400

    removed underscore_params method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user 
 
-
   # def underscore_params!
   #   params.deep_transform_keys!(&:underscore)
   # end
